6f3e0acb658aa19987b88167783dde6ce00ec3ed
Rodrigo <R0drigo-0@users.noreply.github.com>
2024-03-20 20:14:00 +0100

    Ex3

--- ex3.py
+++ ex3.py
@@ -1,13 +1,12 @@
 import os
 import pandas as pd
-from itertools import combinations, product
-from Levenshtein import distance
+from itertools import product
 
-# Función para calcular la distancia de Levenshtein entre dos contraseñas
+
 def password_similarity(pass1, pass2):
     return distance(pass1, pass2)
 
-# Cargar contraseñas
+
 same_user = {}
 folder_path = "pass/"
 files = os.listdir(folder_path)
@@ -30,35 +29,32 @@ for file in files:
 
             user = correo.split("@")[0]
             if user not in same_user:
-                same_user[user] = set()  # Usamos un conjunto para evitar repeticiones
+                same_user[user] = set()
             same_user[user].add(contraseña.strip())
 
-# Analizar contraseñas relacionadas
 related_passwords = []
 for user1, passwords1 in same_user.items():
     if len(passwords1) > 1:
-        related = set()  # Usamos un conjunto para evitar repeticiones
+        related = set()
         for user2, passwords2 in same_user.items():
-            if user1 != user2:  # No comparamos las contraseñas del mismo usuario
+            if user1 != user2:
                 for pass1, pass2 in product(passwords1, passwords2):
                     similarity = password_similarity(pass1, pass2)
-                    # Definir umbral de similitud
-                    if similarity < 3:  # Puedes ajustar este valor según tus necesidades
+                    if similarity < 3:
                         related.add(pass1)
                         related.add(pass2)
         if related:
             related_passwords.append((user1, list(related)))
 
-# Crear un DataFrame con los datos
-df = pd.DataFrame(related_passwords, columns=['Usuario', 'Contraseñas'])
+df = pd.DataFrame(related_passwords, columns=["Usuario", "Contraseñas"])
 
-# Guardar el DataFrame en un archivo Excel
-excel_file = 'password_relations.xlsx'
+excel_file = "password_relations.xlsx"
 df.to_excel(excel_file, index=False)
 
-print(f"Se ha guardado la información de las contraseñas relacionadas en el archivo '{excel_file}'.")
+print(
+    f"Se ha guardado la información de las contraseñas relacionadas en el archivo '{excel_file}'."
+)
 
-# Calcular el porcentaje de contraseñas relacionadas
 total_users = len(same_user)
 users_with_related_passwords = len(df)
 percentage = (users_with_related_passwords / total_users) * 100
